b0acd9cabd51ee87cf765057687ee4786b48fe29
Raj kumar <cj7ps5@gmail.com>
2026-05-05 12:02:02 +0530

    latest commit

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.getElementById('greet-btn').addEventListener('click', () => {
-    document.getElementById('output').textContent = `👋 Hello from ${new Date().getFullYear()}!`;
+    document.getElementById('output').textContent = `👋 
+Kuhugrantha Private Limited (KGPL) is an Indian Private Limited Company incorporated on January 10, 2025, operating as an Information Technology and enterprise services provider. Based in Bhiwandi, Maharashtra, the company specializes in IT infrastructure services, talent acquisition, recruitment, and HR consultancy, boasting a bootstrapped and active status. ${new Date().getFullYear()}!`;
   });
   console.log('✅ Welcome page loaded successfully.');
